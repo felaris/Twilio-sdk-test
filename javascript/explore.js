ea7b693559f15c592d682d5e9cdecbd9dd5942dd
felaris <felixawortwe14@gmail.com>
2023-01-25 16:12:41 +0000

    final commit

--- javascript/explore.js
+++ javascript/explore.js
@@ -1,10 +1,8 @@
-let TWILIO_AUTH_SID;
-let TWILO_AUTH_TOKEN;
 const Twilio = require("twilio");
 
 const client = new Twilio(
-  "AC2a7c23dc33b299ebd312a453c9adb7b8",
-  "bc4ff5f356d4a11a1f18c88d0801b78d"
+  "Your Account SID",
+  "Your Account Token",
 );
 
 client.messages
--- python/explore.py
+++ python/explore.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC2a7c23dc33b299ebd312a453c9adb7b8"
+account_sid = "Your Account SID"
 
 # Your Auth Token from twilio.com/console
-auth_token  = "bc4ff5f356d4a11a1f18c88d0801b78d"
+auth_token  = "Your Auth Token"
 
 client = Client(account_sid, auth_token)
 
